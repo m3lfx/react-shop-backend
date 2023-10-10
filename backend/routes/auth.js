0ef585ccd6c2d11daa519448326f80d6e6295de2
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-10 09:21:03 +0800

    user profile

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, logout, forgotPassword, resetPassword} = require('../controllers/authController');
-router.post('/register',registerUser);
+const { registerUser,
+    loginUser,
+    logout,
+    forgotPassword,
+    resetPassword,
+    getUserProfile
+} = require('../controllers/authController');
+router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/logout',logout);
+router.get('/logout', logout);
 
 router.post('/password/forgot', forgotPassword);
 router.put('/password/reset/:token', resetPassword);
+router.get('/me', isAuthenticatedUser, getUserProfile)
 
 // router.get('/logout',logout);
 
